867cc471d7f675b9a73ec62202081315e3a5c24e
Jankku <29043938+Jankku@users.noreply.github.com>
2021-08-12 14:13:01 +0300

    Added error 401 page

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,7 +1,7 @@
-import { Redirect } from 'react-router-dom';
 import useToken from '../utils/useToken';
+import Error401 from './errors/Error401';
 
 export default function PrivateRoute({ comp: Component, ...rest }) {
   const { token } = useToken();
-  return token ? <Component {...rest} /> : <Redirect to="/login" />;
+  return token ? <Component {...rest} /> : <Error401 />;
 }
---
+++ src/components/errors/Error401.jsx
@@ -0,0 +1,28 @@
+import { Button, Container } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import Header from '../Header';
+
+export default function Error401() {
+  const history = useHistory();
+
+  const redirectToLoginPage = () => {
+    history.replace('/login');
+  };
+
+  return (
+    <>
+      <Header />
+      <Container maxWidth="md" style={{ textAlign: 'center' }}>
+        <h1>401 Unauthorized</h1>
+        <p>Please login before trying to reach this page.</p>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={redirectToLoginPage}
+        >
+          Login
+        </Button>
+      </Container>
+    </>
+  );
+}
