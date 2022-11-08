9e17dc271ac1651f3376712d4d14411176d5ecc1
Jankku <29043938+Jankku@users.noreply.github.com>
2022-11-08 19:30:55 +0200

    feat: share button to share dialog

--- src/components/profile/ShareDialog.jsx
+++ src/components/profile/ShareDialog.jsx
@@ -11,45 +11,61 @@ import { useState } from 'react';
 import { useQuery } from 'react-query';
 import { getShareImage, shareProfile } from '../../data/Profile';
 import useCustomSnackbar from '../../hooks/useCustomSnackbar';
+import { getShareUrl, blobToBase64 } from '../../utils/shareUtil';
 import BaseDialog from '../common/BaseDialog';
 
 function ShareDialog({ visible, closeDialog }) {
-  const [image, setImage] = useState();
   const { showErrorSnackbar, showSuccessSnackbar } = useCustomSnackbar();
+  const [blob, setBlob] = useState();
+  const [imageBase64, setImageBase64] = useState();
+
   const shareProfileQuery = useQuery(['shareProfile'], shareProfile, {
     enabled: visible,
     staleTime: 1000 * 10,
     refetchOnWindowFocus: false,
     onError: (err) => showErrorSnackbar(err.response.data.errors[0].message),
   });
-  const shareId = shareProfileQuery?.data;
 
+  const shareId = shareProfileQuery?.data;
   const imageQuery = useQuery(['shareImage', shareId], () => getShareImage(shareId), {
     enabled: visible && shareId !== undefined,
     staleTime: 0,
     refetchOnWindowFocus: false,
     onSuccess: async (data) => {
-      const base64 = await imgToBase64(data);
-      setImage(base64);
+      setBlob(data);
+      const base64 = await blobToBase64(data);
+      return setImageBase64(base64);
     },
     onError: (err) => showErrorSnackbar(err.response.data.errors[0].message),
   });
 
   const isLoading = shareProfileQuery.isFetching || imageQuery.isFetching;
-  const isCopyButtonDisabled =
+  const isActionDisabled =
     shareProfileQuery.isFetching ||
     shareProfileQuery.isError ||
     imageQuery.isFetching ||
     imageQuery.isError;
 
-  const copyUrlToClipboard = async () => {
-    const baseUrl = process.env.REACT_APP_BASE_URL;
-    const url = new URL(`/api/share/${shareId}`, baseUrl);
+  const onCopyUrl = async () => {
     try {
+      const url = getShareUrl(shareId);
       await navigator.clipboard.writeText(url.toString());
       showSuccessSnackbar('Link copied');
     } catch (error) {
       showErrorSnackbar('Failed to copy link');
+    }
+  };
+
+  const onShare = async () => {
+    const imageFile = new File([blob], shareId, { type: 'image/png' });
+    const shareData = { files: [imageFile] };
+
+    if (!navigator.canShare(shareData)) return showErrorSnackbar('Failed to share image');
+
+    try {
+      await navigator.share(shareData);
+    } catch (error) {
+      showErrorSnackbar('Failed to share image');
     }
   };
 
@@ -61,8 +77,8 @@ function ShareDialog({ visible, closeDialog }) {
           <Grid container justifyContent="center">
             <CircularProgress />
           </Grid>
-        ) : image ? (
-          <img src={image} />
+        ) : imageBase64 ? (
+          <img src={imageBase64} />
         ) : (
           <Typography paragraph>Failed to load image</Typography>
         )}
@@ -72,25 +88,20 @@ function ShareDialog({ visible, closeDialog }) {
           color="secondary"
           onClick={() => {
             closeDialog();
-            setImage();
+            setImageBase64();
           }}
         >
           Cancel
         </Button>
-        <Button color="primary" disabled={isCopyButtonDisabled} onClick={copyUrlToClipboard}>
+        <Button color="primary" disabled={isActionDisabled} onClick={onCopyUrl}>
           Copy link
+        </Button>
+        <Button color="primary" disabled={isActionDisabled} onClick={onShare}>
+          Share
         </Button>
       </DialogActions>
     </BaseDialog>
   );
 }
-
-const imgToBase64 = async (image) => {
-  return new Promise((resolve) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(image);
-    reader.onloadend = (r) => resolve(r.target.result);
-  });
-};
 
 export default ShareDialog;
---
+++ src/utils/shareUtil.js
@@ -0,0 +1,12 @@
+export const getShareUrl = (shareId) => {
+  const baseUrl = process.env.REACT_APP_BASE_URL;
+  return new URL(`/api/share/${shareId}`, baseUrl);
+};
+
+export const blobToBase64 = async (image) => {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.onloadend = (r) => resolve(r.target.result);
+  });
+};
