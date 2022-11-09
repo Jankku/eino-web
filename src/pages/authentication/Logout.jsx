9fa225b34db69d99f7422a40085e85b58547c313
Jankku <29043938+Jankku@users.noreply.github.com>
2022-11-09 10:20:54 +0200

    fix: clear query cache on log-out

--- src/pages/authentication/Logout.jsx
+++ src/pages/authentication/Logout.jsx
@@ -1,11 +1,16 @@
 import useToken from '../../hooks/useToken';
 import { Navigate } from 'react-router-dom';
 import { useAuthContext } from '../../providers/AuthenticationProvider';
+import { useQueryClient } from 'react-query';
 
 export default function Logout() {
   const { removeTokens } = useToken();
   const { setIsLoggedIn } = useAuthContext();
+  const queryClient = useQueryClient();
+
   setIsLoggedIn(false);
   removeTokens();
+  queryClient.clear();
+
   return <Navigate to={'/'} replace />;
 }
