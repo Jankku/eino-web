d99ffc85ece0e00a6f36e483f3024289afe8967b
Jankku <29043938+Jankku@users.noreply.github.com>
2023-02-19 22:25:04 +0200

    fix: RequireAuth

--- src/components/common/RequireAuth.jsx
+++ src/components/common/RequireAuth.jsx
@@ -2,6 +2,6 @@ import useToken from '../../hooks/useToken';
 import Error401 from '../errors/Error401';
 
 export default function RequireAuth({ children }) {
-  const { token } = useToken();
-  return token ? children : <Error401 />;
+  const { isRefreshTokenValid } = useToken();
+  return isRefreshTokenValid() ? children : <Error401 />;
 }
