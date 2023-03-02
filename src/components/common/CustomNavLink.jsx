0ebf91afe8e2a0ab9cb43b76bded0b1fe2f0703f
Jankku <29043938+Jankku@users.noreply.github.com>
2023-03-02 15:10:46 +0200

    refactor: simpliy navlink

--- src/components/common/CustomNavLink.jsx
+++ src/components/common/CustomNavLink.jsx
@@ -1,25 +1,25 @@
-import { ListItemText, MenuItem, useTheme } from '@mui/material';
-import { NavLink, useMatch, useResolvedPath } from 'react-router-dom';
+import { ListItemText, MenuItem } from '@mui/material';
+import { NavLink } from 'react-router-dom';
 
 function CustomNavLink({ item }) {
-  const theme = useTheme();
-  const resolved = useResolvedPath(item.path);
-  const match = useMatch({ path: resolved.pathname, end: item.path === '/' });
-
   return (
     <NavLink to={item.path}>
-      <MenuItem
-        selected={match ? true : false}
-        sx={
-          match && {
-            bgcolor: `${theme.palette.primary.light} !important`,
-            color: `${theme.palette.primary.contrastText} !important`,
+      {({ isActive }) => (
+        <MenuItem
+          selected={isActive}
+          sx={(theme) =>
+            isActive
+              ? {
+                  bgcolor: `${theme.palette.primary.light} !important`,
+                  color: `${theme.palette.primary.contrastText} !important`,
+                }
+              : null
           }
-        }
-      >
-        {item.icon}
-        <ListItemText primary={item.name} primaryTypographyProps={{ fontWeight: 500 }} />
-      </MenuItem>
+        >
+          {item.icon}
+          <ListItemText primary={item.name} primaryTypographyProps={{ fontWeight: 500 }} />
+        </MenuItem>
+      )}
     </NavLink>
   );
 }
