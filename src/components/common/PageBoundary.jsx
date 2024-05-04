21e28ebdd9690e8599e89e7eb093f284f486552e
Jankku <29043938+Jankku@users.noreply.github.com>
2024-05-04 23:05:51 +0300

    feat: show suspense spinner after delay

--- src/components/common/PageBoundary.jsx
+++ src/components/common/PageBoundary.jsx
@@ -1,8 +1,24 @@
 import { CircularProgress, Stack } from '@mui/material';
 import { QueryErrorResetBoundary } from '@tanstack/react-query';
-import { Suspense } from 'react';
+import { Suspense, useState, useEffect } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import PageFallback from './PageFallback';
+
+function Wait({ children }) {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setReady(true);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  return <>{ready ? children : undefined}</>;
+}
 
 function PageBoundary({ children }) {
   return (
@@ -11,9 +27,11 @@ function PageBoundary({ children }) {
         <ErrorBoundary onReset={reset} FallbackComponent={PageFallback}>
           <Suspense
             fallback={
-              <Stack justifyContent="center" alignItems="center" mt={10}>
-                <CircularProgress />
-              </Stack>
+              <Wait>
+                <Stack justifyContent="center" alignItems="center" mt={10}>
+                  <CircularProgress />
+                </Stack>
+              </Wait>
             }
           >
             {children}
