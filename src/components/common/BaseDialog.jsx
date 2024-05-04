1ef1b0950ba8ef621a95a12b48ed8dda47977411
Jankku <29043938+Jankku@users.noreply.github.com>
2024-05-04 22:28:26 +0300

    feat: tab close warning when dialog open

--- src/components/common/BaseDialog.jsx
+++ src/components/common/BaseDialog.jsx
@@ -1,9 +1,26 @@
 import { useTheme } from '@mui/system';
 import { Dialog, useMediaQuery } from '@mui/material';
+import { useEffect } from 'react';
 
 function BaseDialog({ children, ...props }) {
   const theme = useTheme();
   const fullscreenBelowMd = useMediaQuery(theme.breakpoints.down('md'));
+  const open = props.open;
+
+  useEffect(() => {
+    if (!open) return;
+
+    const handleBeforeUnload = (event) => {
+      event.preventDefault();
+      event.returnValue = '';
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [open]);
 
   return (
     <Dialog {...props} fullScreen={fullscreenBelowMd}>
