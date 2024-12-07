848cd48588631317e5014631b06496444952e145
Jankku <29043938+Jankku@users.noreply.github.com>
2024-12-07 16:36:08 +0200

    feat: theme change synced between multiple tabs

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,8 +1,29 @@
-import { createContext, useContext, useState, useMemo, ReactNode } from 'react';
+import {
+  createContext,
+  useContext,
+  useMemo,
+  ReactNode,
+  useSyncExternalStore,
+  useCallback,
+} from 'react';
 import { CssBaseline, ThemeProvider as MuiThemeProvider, useMediaQuery } from '@mui/material';
 import { StyledEngineProvider } from '@mui/system';
 import { dark } from '../themes/dark';
 import { light } from '../themes/light';
+
+const THEME_KEY = 'dark_theme';
+
+const getThemeFromLocalStorage = (defaultValue: boolean) => () => {
+  const theme = localStorage.getItem(THEME_KEY);
+  return theme === null ? defaultValue : JSON.parse(theme);
+};
+
+const subscribe = (callback: () => void) => {
+  window.addEventListener('storage', callback);
+  return () => {
+    window.removeEventListener('storage', callback);
+  };
+};
 
 const ThemeContext = createContext<{ isDark: boolean; toggleTheme: () => void }>({
   isDark: false,
@@ -11,33 +32,30 @@ const ThemeContext = createContext<{ isDark: boolean; toggleTheme: () => void }>
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const [isDark, setIsDark] = useState(() => {
-    const value = localStorage.getItem('dark_theme');
-    if (value !== null) {
-      return JSON.parse(value) === true;
-    } else {
-      return prefersDarkMode;
-    }
-  });
+  const getTheme = useCallback(
+    () => getThemeFromLocalStorage(prefersDarkMode)(),
+    [prefersDarkMode],
+  );
+  const isDark = useSyncExternalStore(subscribe, getTheme);
 
-  const toggleTheme = () => {
-    setIsDark((prev) => {
-      localStorage.setItem('dark_theme', !prev as unknown as string);
-      return !prev;
-    });
-  };
+  const toggleTheme = useCallback(() => {
+    const previousTheme = localStorage.getItem(THEME_KEY);
+    const newValue = previousTheme === null ? !prefersDarkMode : !JSON.parse(previousTheme);
+    localStorage.setItem(THEME_KEY, JSON.stringify(newValue));
+    window.dispatchEvent(new Event('storage'));
+  }, [prefersDarkMode]);
 
   const theme = useMemo(() => (isDark ? dark : light), [isDark]);
 
   return (
-    <ThemeContext.Provider value={{ isDark, toggleTheme }}>
+    <ThemeContext value={{ isDark, toggleTheme }}>
       <StyledEngineProvider injectFirst>
         <MuiThemeProvider theme={theme}>
           <CssBaseline enableColorScheme />
           {children}
         </MuiThemeProvider>
       </StyledEngineProvider>
-    </ThemeContext.Provider>
+    </ThemeContext>
   );
 }
 
