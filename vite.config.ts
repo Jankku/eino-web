c72058ee2d3ab194f63a722226e03b2d1cd8d855
Jankku <29043938+Jankku@users.noreply.github.com>
2024-12-20 23:14:23 +0200

    build: brotli compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
+import compress from 'vite-plugin-compression';
 import * as child from 'child_process';
 
 const commitHash = child.execSync('git rev-parse --short HEAD').toString();
@@ -12,6 +13,7 @@ export default defineConfig(() => ({
   plugins: [
     react(),
     checker({ typescript: { tsconfigPath: 'tsconfig.app.json' }, overlay: false }),
+    compress({ algorithm: 'brotliCompress', verbose: false }),
   ],
   server: {
     open: true,
