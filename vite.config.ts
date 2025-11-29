a41c0f9049d567d944d2c46951fad41010945033
Jankku <29043938+Jankku@users.noreply.github.com>
2025-11-29 20:20:40 +0200

    chore: enable react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,11 @@ export default defineConfig(() => ({
     __COMMIT_HASH__: JSON.stringify(commitHash),
   },
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
     checker({ typescript: { tsconfigPath: 'tsconfig.app.json' }, overlay: false }),
     compress({ algorithm: 'brotliCompress', verbose: false }),
   ],
