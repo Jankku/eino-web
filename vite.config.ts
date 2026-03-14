f94790c48ab3a7b1f861af6821b21d1ce2b60ecf
Jankku <29043938+Jankku@users.noreply.github.com>
2026-03-14 13:44:50 +0200

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
 import checker from 'vite-plugin-checker';
 import compress from 'vite-plugin-compression';
 import * as child from 'child_process';
@@ -11,11 +12,8 @@ export default defineConfig(() => ({
     __COMMIT_HASH__: JSON.stringify(commitHash),
   },
   plugins: [
-    react({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
-    }),
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
     checker({ typescript: { tsconfigPath: 'tsconfig.app.json' }, overlay: false }),
     compress({ algorithm: 'brotliCompress', verbose: false }),
   ],
